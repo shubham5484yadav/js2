8e11e0b1b41775abdd5ef51362755243299dcd8e
shubham5484yadav <148310106+shubham5484yadav@users.noreply.github.com>
2026-02-01 19:27:57 +0000

    dataTypes

---
+++ basic/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat al js code as newer version
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3+
+    3); // code readability shoudld be high
+
+console.log("shubhan");
+
+
+let name = "shubham"
+let age = 21
+let isLogIn = false
+let state;
+
+// number => 2 to power 53 
+// bigInt 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object 
+console.log(typeof null); object
+console.log(typeof undefined); // undefined
+
+
+
