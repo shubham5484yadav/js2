9ea7f0a2337b4e3a7bb2860ac1fada8907086ac5
shubham5484yadav <148310106+shubham5484yadav@users.noreply.github.com>
2026-02-02 16:34:02 +0000

    datatype-summary

---
+++ basic/datatypes-summary.js
@@ -0,0 +1,36 @@
+// primitive
+
+// 7 types : String, Number, boolean , null, undefined, Symbol, BigInt
+const score = 100 //  data type of score = number
+const scoreValue = 100.3 // type of scoreValue = number
+
+const isloggedIn = false // data type of isloggedIn = undefined
+const outsideTemp = null // data type of outsideTemp = object
+let userEmail; //  data type of userEmail = undefined
+
+const id = Symbol('123')  // data type of id = symbol 
+const anotherId = Symbol('123') // data type of antherId = symbol
+
+console.log(id===anotherId);
+const bigNumber = 23443557896898765432n // data type of bigNumber = bigInt
+
+
+
+// Reference (non primitive)
+
+// Array, object, Functions 
+
+const heros = ["Shaktiman","Naagraj","doga"]  //  data  type of heros = object
+let myObj = {
+    name: "shubham",            // data type of myObj = object
+    age: 22,  
+    } 
+
+const myFunction = function(){
+    console.log("hello world");     // data type of myFunction = function 
+    
+} 
+ 
+console.log(typeof bigNumber);
+console.log(typeof bigNumber);
+
