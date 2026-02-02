3b52517ef6fed806a960d3ba9cba4c61b8072b5e
shubham5484yadav <148310106+shubham5484yadav@users.noreply.github.com>
2026-02-02 17:14:58 +0000

    datatypes-summary

--- basic/datatypes-summary.js
+++ basic/datatypes-summary.js
@@ -31,6 +31,29 @@ const myFunction = function(){
     
 } 
  
-console.log(typeof bigNumber);
-console.log(typeof bigNumber);
+// console.log(typeof bigNumber);
+// console.log(typeof bigNumber);
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+let myYoutubename = "hiteshchoudharydotcom"
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "hitesh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
 
