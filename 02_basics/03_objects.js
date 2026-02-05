be70960443fe71459535abd459dc0be7933e93ec
shubham5484yadav <148310106+shubham5484yadav@users.noreply.github.com>
2026-02-05 21:57:23 +0000

    objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,20 @@
+// singleton
+//Object.create
+
+// object literals 
+
+const JsUser = {
+    name: "Hitesh",
+    "full name" : "Hitesh Choudhary",
+    age: 18,
+    location: "jaipur",
+    email: "hitesh@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday","Saturday"]
+
+}
+
+console.log(JsUser.email);
+console.log(JsUser["email"]);
+
+
