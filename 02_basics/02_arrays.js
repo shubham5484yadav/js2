75412421463ca9d7f62e20d714d6beb2c4d6c763
shubham5484yadav <148310106+shubham5484yadav@users.noreply.github.com>
2026-02-12 08:40:15 +0000

    arrays

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -1,7 +1,9 @@
 const marvel_heros = ["thor","Ironman","spiderman"]
-const dc_heros = ["superma","flash","batman"]
+const dc_heros = ["superman","flash","batman"]
 
 //marvel_heros.push(dc_heros)
+//console.log(marvel_heros);
+
 //console.log(marvel_heros[3][1]);
 
 //const allHeros = marvel_heros.concat(dc_heros)
@@ -17,9 +19,9 @@ const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
 const real_another_array = another_array.flat(Infinity)
 //console.log(real_another_array);
 
-console.log(Array.isArray("Hitesh"));
-console.log(Array.from("Hitesh"));
-console.log(Array.from({name: "hitesh"}));  // intresting
+//console.log(Array.isArray("Hitesh"));
+//console.log(Array.from("Hitesh"));
+//console.log(Array.from({name: "hitesh"}));  // intresting
 
 
 let score1 = 100
