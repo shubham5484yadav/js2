5315d9bae4cab9e3a2bf1044e6c6659fb4ba297b
shubham5484yadav <148310106+shubham5484yadav@users.noreply.github.com>
2026-02-21 18:16:47 +0000

    control_statement

---
+++ 04_control_flow/one.js
@@ -0,0 +1,28 @@
+// if
+
+// const temperature = 41
+
+// if(temperature === 41){
+//     console.log("less than 50");
+    
+// }  else{
+//     console.log("Temperature is greater than 50");
+    
+// }
+
+// console.log("Executed");
+
+// <,>,<=,>=,==,!=,===,!==
+
+// const score = 200
+// if(score>100 ){
+//     const power = "fly"
+//     console.log(`User power $(power)`);
+    
+// }
+
+const balance = 1000
+if(balance >500) console.log("test");
+console.log("test2");
+
+
